6615439f7176e1ba924ff9f7f0ee4a4bf084a48b
B phan <bphan@Bs-MacBook-Air.local>
2022-11-04 16:05:29 -0600

    added jwt for registration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
+/////////////// Import dependencies ///////////////
 const express = require("express");
-const mongodb = require("mongodb");
+const cors = require('cors');
+// const mongodb = require("mongodb");
+const mongoose = require('mongoose');
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const jsonwebtoken = require('jsonwebtoken');
+/////////////////////////////////////////////////
 
+
+///////////////////////////////////////////// Configurations ////////////////////////////////////////
 const app = express();
+
 dotenv.config();
 
+const port = process.env.PORT || 5000;
+
+const username = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+const mongoURI = "mongodb+srv://" + username + ":" + password + "@cluster0.dxhaxm8.mongodb.net/?retryWrites=true&w=majority";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* Test database connection
 const listDatabases = async (client) => {
   const databasesList = await client.db().admin().listDatabases();
   console.log("Databases:");
@@ -30,4 +48,58 @@ const run = async () => {
 }
 
 run().catch(console.error);
+*/
+
+
+//////////////////////////// Database connection /////////////////////////
+mongoose.connect(mongoURI)
+.then(
+	() => {console.log('Connected successfully to mongodb.');},
+	(err) => {console.log(`Failed to connect to mongodb. Error: ${err}`);}
+);
+/////////////////////////////////////////////////////////////////////////
+
+
+///////////////////// Import routes ////////////////////
+const userRouter = require('./routes/user.route');
+///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////// Middleware /////////////////////////////////////////////
+app.use(cors);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+		jsonwebtoken.verify(
+			req.headers.authorization.split(' ')[1],
+			'RESTFULAPIs',
+			(err, decode) => {
+				if (err) {req.user = undefined;}
+				else {req.user = decode;}
+				next();
+			}
+		);
+	}
+	else {
+		req.user = undefined;
+		next();
+	}
+});
+
+app.use('/api/users', userRouter);
+
+app.use((req, res) => {
+	res.status(404).send({url: req.originalUrl + 'not found'});
+});
+
+app.listen(port, () => {console.log('RESTful API server started on: ' + port + '.');});
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+///////// Exports /////////
 module.exports = app;
+//////////////////////////
