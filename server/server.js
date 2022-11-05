1aa2bdf47a9ccc2257a64e71ced940b339b8d98a
TonyPhan0505 <87828913+TonyPython0505@users.noreply.github.com>
2022-11-05 17:01:01 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
 	}
 });
 
-app.use('/users', userRouter);
+app.use('/user', userRouter);
 
 app.use((req, res) => {
 	res.status(404).send({url: req.originalUrl + 'not found'});
