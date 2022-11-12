f319433ccc2d725b02b802851d556360eaf5ec0c
TonyPhan0505 <87828913+TonyPython0505@users.noreply.github.com>
2022-11-12 14:55:58 -0700

    Update OnRegisterTokenVerifier.js

--- server/services/OnRegisterTokenVerifier.js
+++ server/services/OnRegisterTokenVerifier.js
@@ -9,7 +9,7 @@ exports.verify = (username, role, token, secretKey, res) => {
 	if (!token) {res.status(401).json({message: 'Undefined token during registration.'});}
 	else {
 		jwt.verify(token, secretKey, (err, user) => {
-			if (err) {res.status(400).json({message: `Token verification failed during registration. Error: ${err}.`});}
+			if (err) {res.status(400).json({message: `Invalid token during registration. ${err}.`});}
 			else {
 				NewUserCreator.createUser(username, role, res);
 			}
