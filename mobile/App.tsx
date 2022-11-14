5b2db5baeb0e1f67c513a649d165f564a38c6b16
kalliehuynh <kallie1@ualberta.ca>
2022-11-14 16:24:48 -0700

    added login routing with snapshot testing for the homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Homepage from './components/home/Homepage';
 import Login from './components/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Login/>
+      {/* <Login/> */}
+      <Homepage />
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/components/home/CaregiverHome.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function CaregiverHome() {
+    return (
+        <View>
+            <Text>Caregiver Homepage</Text>
+        </View>
+    )
+}
+
