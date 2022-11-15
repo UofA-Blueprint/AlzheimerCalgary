4ce5fe8a170c9c9a8741e9cecd0434f3a21de149
Sooraj-beep <soorajarakkal@gmail.com>
2022-11-14 19:39:23 -0700

    require mongoose

--- server/tst/user.route.test.js
+++ server/tst/user.route.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // const app = require('../server');
 const dotenv = require("dotenv");
 dotenv.config();
