2e7be82ba2172e00c56ec383c6dfbc5b18bc1182
Sooraj-beep <soorajarakkal@gmail.com>
2022-11-14 21:16:25 -0700

    removed commented out code

--- server/app.js
+++ server/app.js
@@ -20,33 +20,6 @@ const password = process.env.MONGO_PASSWORD;
 const mongoURI = "mongodb+srv://" + username + ":" + password + "@cluster0.dxhaxm8.mongodb.net/?retryWrites=true&w=majority";
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-/* Test database connection
-const mongodb = require("mongodb");
-const listDatabases = async (client) => {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-const run = async () => {
-  const username = process.env.MONGO_USER;
-  const password = process.env.MONGO_PASSWORD;
-  const uri = "mongodb+srv://" + username + ":" + password + "@cluster0.dxhaxm8.mongodb.net/?retryWrites=true&w=majority";
-  const client = new mongodb.MongoClient(uri);
-  try {
-    // Connect to MongoDB cluster
-    await client.connect();
-    await listDatabases(client);
-  } catch (error) {
-    console.error(error);
-  } finally{
-    await client.close();
-  }
-}
-run().catch(console.error);
-*/
-
-
 //////////////////////////// Database connection /////////////////////////
 mongoose.connect(mongoURI)
 .then(
