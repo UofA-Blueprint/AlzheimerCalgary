b32b176c79a9df55bec0a6d5a12ad3da31feb585
B phan <bphan@Bs-MacBook-Air.local>
2023-01-03 16:47:57 -0700

    add media component

--- mobile/components/home/Homepage.tsx
+++ mobile/components/home/Homepage.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import StaffHome from './StaffHome';
 import CaregiverHome from './CaregiverHome';
 import ClientHome  from './ClientHome';
+import { TouchableOpacity } from 'react-native';  // to test the add media pop up component
+import AddMediaPopUp from '../media/AddMediaPopUp';  // to test the add media pop up component
 
 interface RouterProps {
   role: string
@@ -28,12 +30,33 @@ const RoleRouter = ({role}: RouterProps) => {
 
 const Homepage = () => {
     const [userRole, setUserRole] = useState('caregiver');
+    const [addMediaPopUpVisible, setAddMediaPopUpVisible] = useState(false);
 
     return (
         <View>
             <Text>Homepage</Text>
             <Text>Your role: {userRole}</Text>
             <RoleRouter role={userRole}/>
+            {
+                <TouchableOpacity 
+                    onPress={() => {setAddMediaPopUpVisible(true);}}
+                    style={{
+                        width: 150,
+                        height: 50,
+                        backgroundColor: 'black',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        marginTop: 30,
+                        borderRadius: 10,
+                    }}>
+                    <Text style={{fontSize: 15, fontWeight: 'bold', color: 'white'}}>Add Media/File</Text>
+                </TouchableOpacity>
+            }
+            {
+                addMediaPopUpVisible ?
+                    <AddMediaPopUp setVisible={setAddMediaPopUpVisible}/>
+                : null
+            }
         </View>
     )
 };
---
+++ mobile/components/media/AddMediaPopUp.tsx
@@ -0,0 +1,132 @@
+////////// Import Dependencies //////////
+import { useState } from 'react';
+import * as React from 'react';
+import { StyleSheet, Modal, View, Image, TouchableOpacity, Text} from 'react-native';
+import * as MediaPicker from 'expo-image-picker';
+import VideoPlayer from 'react-native-video-controls';
+import { EvilIcons, Feather } from '@expo/vector-icons';
+////////////////////////////////////////
+
+/////////// Configurations ///////////
+interface Props {
+	setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+//////////////////////////////////////
+
+//////////////// Component //////////////
+export default function AddMediaPopUp({ setVisible }: Props) {
+	/* 
+		Props explanation:
+			+) visible: this is a boolean prop. Where AddMediaPopUp is used, visible will be a state.
+				Its initial state is false. When a 'Add Media' button somewhere is pressed to open this pop up, visible will be set to true.  
+	*/
+
+	// States //
+	const [mediaURI, setMediaURI] = useState('');
+	const [mediaType, setMediaType] = useState<string | undefined>('');
+	///////////
+
+	// Callbacks //
+	const pickMedia = async () => {
+		let result = await MediaPicker.launchImageLibraryAsync({
+			mediaTypes: MediaPicker.MediaTypeOptions.All,
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+		if (!result.cancelled) {
+			setMediaURI(result.uri);
+			setMediaType(result.type);
+		}
+	}
+	/////////////
+
+	return (
+		<Modal transparent>
+			<View style={styles.popUpOuterFrame}>
+				<View style={styles.popUpInnerFrame}>
+					<View style={styles.closeFrame}>
+						<TouchableOpacity onPress={() => {setVisible(false)}}><EvilIcons name="close" size={38} color="grey" /></TouchableOpacity>
+					</View>
+					{
+						//<Image source={{ uri: mediaURI }} style={styles.image}/>
+						mediaURI && mediaType === "image" ?
+							<View style={{width: '90%', height: 220}}>
+								<Text>{mediaURI}</Text>
+							</View>
+						: null
+					}
+					{
+						// <VideoPlayer controls={true} source={{ uri: mediaURI }} style={styles.video}/>
+						mediaURI && mediaType === "video" ?
+							<View style={{width: '90%', height: 220}}>
+								<Text>{mediaURI}</Text>
+							</View>
+						: null
+					}
+					{
+						!mediaURI ?
+							<Feather name="camera-off" size={150} color="#C5C6D0" />
+						: null
+					}
+					<TouchableOpacity style={styles.addMediaButton} onPress={pickMedia}>
+						<Text style={styles.addMediaText}>Add Media/File</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
+		</Modal>
+	);
+}
+////////////////////////////////////////
+
+///////////////// Styles ////////////////
+const styles = StyleSheet.create({
+	popUpOuterFrame: {
+		flex : 1,
+		backgroundColor: 'rgba(0,0,0,0.5)',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	popUpInnerFrame: {
+		width: 350,
+		backgroundColor: 'white',
+		alignItems: 'center',
+		borderRadius: 15,
+	},
+
+	closeFrame: {
+		width: '100%',
+		height: 40,
+		justifyContent: 'center',
+		alignItems: 'flex-end',
+		paddingRight: 5,
+		marginBottom: 60,
+	},
+
+	addMediaText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: 'white',
+	},
+
+	addMediaButton: {
+		width: '70%',
+		height: 50,
+		marginTop: 40,
+		marginBottom: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 22,
+		backgroundColor: 'black',
+	},
+
+	image: {
+		flex: 1,
+	},
+	
+	video: {
+		flex: 1,
+	},
+});
+////////////////////////////////////////
