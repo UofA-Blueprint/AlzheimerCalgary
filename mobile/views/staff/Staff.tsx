373784f39580804d7a812b338953978621d35fb7
B Phan <bphan@Bs-MacBook-Pro.local>
2023-01-18 15:14:13 -0700

    updates-to-staff-management-screen

--- mobile/views/staff/Staff.tsx
+++ mobile/views/staff/Staff.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { 
   Text, 
   ScrollView,
@@ -5,12 +6,173 @@ import {
   StyleSheet, 
   TouchableOpacity, 
   TextInput,
-  StatusBar } from 'react-native'
+  StatusBar,
+  Dimensions } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context"
 import { Ionicons } from '@expo/vector-icons'
 import { Feather } from '@expo/vector-icons';
 
 export default function Staff(prop: any) {
+  const Departments = [
+    {
+      id: 1,
+      name: 'Finance'
+    },
+
+    {
+      id: 2,
+      name: 'Customer Service'
+    },
+
+    {
+      id: 3,
+      name: 'Activities Management'
+    },
+
+    {
+      id: 4,
+      name: 'Management'
+    },
+  ]
+
+  const OnDuty = [
+    {
+      username: 'Tony Phan',
+      start_time: '2022-02-26T08:37:48.244Z',
+      leave_time: '2022-02-26T16:37:48.244Z'
+    },
+
+    {
+      username: 'Tom Holland',
+      start_time: '2022-02-26T08:37:48.244Z',
+      leave_time: '2022-02-26T16:37:48.244Z',
+    },
+
+    {
+      username: 'Tanya Boothe',
+      start_time: '2022-02-26T08:37:48.244Z',
+      leave_time: '2022-02-26T16:37:48.244Z'
+    },
+
+    {
+      username: 'Peggy Ross',
+      start_time: '2022-02-26T08:37:48.244Z',
+      leave_time: '2022-02-26T16:37:48.244Z'
+    },
+
+    {
+      username: 'Jennifer Smith',
+      start_time: '2022-02-26T08:37:48.244Z',
+      leave_time: '2022-02-26T16:37:48.244Z'
+    },
+  ]
+
+  const Staff = [
+    {
+      username: 'John Smith',
+      role: 'staff',
+      department: 1,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-01-01'
+    },
+
+    {
+      username: 'Clark Kent',
+      role: 'staff',
+      department: 2,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-01'
+    },
+
+    {
+      username: 'Lucas Milton',
+      role: 'staff',
+      department: 3,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-15'
+    },
+
+    {
+      username: 'Tom Holland',
+      role: 'staff',
+      department: 4,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-09'
+    },
+
+    {
+      username: 'Tanya Boothe',
+      role: 'staff',
+      department: 1,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-05-01'
+    },
+
+    {
+      username: 'Tony Phan',
+      role: 'staff',
+      department: 2,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-05-05'
+    },
+
+    {
+      username: 'Peggy Ross',
+      role: 'staff',
+      department: 3,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-15'
+    },
+
+    {
+      username: 'Nicole Collins',
+      role: 'staff',
+      department: 4,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-09'
+    },
+
+    {
+      username: 'Mark Jamison',
+      role: 'staff',
+      department: 1,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-01-01'
+    },
+
+    {
+      username: 'Frank Lampard',
+      role: 'staff',
+      department: 2,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-01'
+    },
+
+    {
+      username: 'Jennifer Smith',
+      role: 'staff',
+      department: 3,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-15'
+    },
+
+    {
+      username: 'Richard Holland',
+      role: 'staff',
+      department: 4,
+      hash_password: 'a;rgijae;ogjao;erigj',
+      created: '2023-02-09'
+    },
+
+  ]
+
+  const [ filteredData, setFilteredData ] = useState(Staff)
+
+  const staffViewWidth = Dimensions.get('window').width / 4
+  const staffPerRow = (Dimensions.get('window').width - 60) / staffViewWidth  // 60: hypothetical total horizontal padding of the ScrollView
+  const staffRows = filteredData.length / staffPerRow
+
+
   function addStaff() {}
 
   function showStaffFiltersPopUp() {}
@@ -32,21 +194,23 @@ export default function Staff(prop: any) {
           </TouchableOpacity>
         </View>
       </View>
-      <ScrollView>
-        <View style={styles.searchAndFilterFrame}>
-          <View style={styles.searchOuterFrame}>
-            <View style={styles.searchInnerFrame}>
-              <Feather name="search" size={30} color="white" />
-              <TextInput style={styles.searchInputField} placeholder="Search staff" placeholderTextColor='grey' autoCorrect={false}/>
-            </View>
-          </View>
-          <View style={styles.filterButtonFrame}>
-            <TouchableOpacity style={styles.filterButton} onPress={showStaffFiltersPopUp}>
-              <Ionicons name="filter" size={30} color="black" />
-              <Text style={styles.filterButtonText}>Filters</Text>
-            </TouchableOpacity>
+      <View style={styles.searchAndFilterFrame}>
+        <View style={styles.searchOuterFrame}>
+          <View style={styles.searchInnerFrame}>
+            <Feather name="search" size={30} color="white" />
+            <TextInput style={styles.searchInputField} placeholder="Search staff" placeholderTextColor='grey' autoCorrect={false}/>
           </View>
         </View>
+        <View style={styles.filterButtonFrame}>
+          <TouchableOpacity style={styles.filterButton} onPress={showStaffFiltersPopUp}>
+            <Ionicons name="filter" size={30} color="black" />
+            <Text style={styles.filterButtonText}>Filters</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.filtersFrame}></View>
+      <ScrollView>
+
       </ScrollView>
     </SafeAreaView>
   )
@@ -145,4 +309,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginLeft: 15,
   },
+
+  filtersFrame: {},
 })
